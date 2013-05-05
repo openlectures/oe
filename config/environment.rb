cb639f1b1fedd7d58b231d7e3cf5d129fc3056be
Jethro Kuan <jethrokuan95@gmail.com>
2013-05-05 20:01:19 +0800

    Hopefully added Bower support

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application.
 require File.expand_path('../application', __FILE__)
 
+Oe::Application.configure do
+  config.assets.append_path 'components'
+end
+
 # Initialize the rails application.
 Oe::Application.initialize!
